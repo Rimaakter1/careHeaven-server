3de45120e22f276a1d391b5070b18d1f9b6b1a53
rimaakter1 <rimac201262@gmail.com>
2025-01-26 02:38:21 +0600

    added delete method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const morgan = require('morgan')
@@ -118,6 +118,13 @@ async function run() {
         const result = await campsCollection.find().toArray()
             res.send(result)
        })
+
+        app.delete('/camps/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await campsCollection.deleteOne(query)
+            res.send(result)
+        })
 
         app.post('/jwt', async (req, res) => {
             const email = req.body
